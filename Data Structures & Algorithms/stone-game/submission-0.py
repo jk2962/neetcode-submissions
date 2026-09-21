0d10d728ab2f2424d97b36251637f8e323a29aa0
Jason Kim <jk2962@cornell.edu>
2026-09-21 10:17:18 -0400

    Add: stone-game - submission-0

---
+++ Data Structures & Algorithms/stone-game/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        piles.sort()
+        alice_count = 0 
+        bob_count = 0
+        for i in range(len(piles)):
+            return True
+
